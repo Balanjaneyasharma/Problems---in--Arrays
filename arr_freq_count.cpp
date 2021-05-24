f48ca10ba83d08082a9ddad167413f93563cc455
Balanjaneyasharma <69688211+Balanjaneyasharma@users.noreply.github.com>
2021-05-24 15:07:24 +0530

    Add files via upload

---
+++ arr_freq_count.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+#include<map>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	map<int,int>d;
+	for(int i=0;i<n;i++)
+	{
+		d[arr[i]]++;
+	}
+	int id,maxi=0;
+	for(auto it:d)
+	{
+		if (maxi<it.second)
+		{
+			maxi=it.second;
+			id=it.first;
+		}
+	}
+	cout<<"Customer id : "<<id<<" And its count : "<<maxi<<endl;
+	
+}
